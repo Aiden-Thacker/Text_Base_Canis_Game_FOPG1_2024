b026e9c178e036bf0ebf620618c2bb8620c03353
Xander Adams <xanderadams555@gmail.com>
2024-03-20 17:22:51 -0500

    Combat 0.0.0.2

--- src/Character.hpp
+++ src/Character.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
 #include "Entity.hpp"
+#include "Stats.hpp"
+#include "Dice.hpp"
+#include "Shield.hpp"
+#include "Weapon.hpp"
+#include "Armor.hpp"
 
 class Character : public Entity
 {
-
+    public:
+    Stats stats;
+    float health = stats.maxHealth;
+    Shield shield;
+    Weapon weapon;
+    Armor armor;
 };
